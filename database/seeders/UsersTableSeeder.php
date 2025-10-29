15831b6cdf208fee2e4704f1cdb0cafba2d9f1bf
lhernamirafuenteraquinio-svg <lhernamirafuenteraquinio@gmail.com>
2025-10-29 13:24:03 +0800

    fix namespace declaration

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\support\facades\Hash;
+use Illuminate\Support\Facades\Hash;
 
 	use App\Models\Role;
 	use App\Models\User;
