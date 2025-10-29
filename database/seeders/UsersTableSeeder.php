1638383add766c4f433b099c53296555dad38fb9
lhernamirafuenteraquinio-svg <lhernamirafuenteraquinio@gmail.com>
2025-10-29 14:06:13 +0800

    fixed spaces

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,11 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-
-	use App\Models\Role;
-	use App\Models\User;
-
-	use DB;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
